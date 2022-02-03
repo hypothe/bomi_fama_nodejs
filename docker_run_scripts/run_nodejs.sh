8d5e3d44dd7e5f08569b88d54e32c162744cb062
hypothe <markeagle98@gmail.com>
2022-02-03 11:13:02 +0100

    <Add> proper container networking

--- docker_run_scripts/run_nodejs.sh
+++ docker_run_scripts/run_nodejs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -it --rm -p 8080:4242 hypothe/node_bomi_fama
+# Create network named 'bomi-fama' if it doesn't exist yet
+NET_NAME="bomi-fama"
+
+docker network inspect ${NET_NAME} --format {{.Id}} 2>/dev/null || docker network create --driver bridge ${NET_NAME}
+
+docker run -it --rm -p 8080:4242 --network=${NET_NAME} --name=bomi_server hypothe/node_bomi_fama
